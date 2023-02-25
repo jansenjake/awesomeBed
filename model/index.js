d500fc9a163f285eec47f2e609197c91e70f5e37
jakejansen <jansenjake599@gmail.com>
2023-02-25 13:57:55 +0200

    Update on the model

--- model/index.js
+++ model/index.js
@@ -64,142 +64,75 @@ SELECT userID,firstName,lastName ,emailAddress,userPass,gender,userRole,userProf
   }
 }
 
+class Product {
+  fetchProducts(req, res) {
+      const strQry = `SELECT id, prodName, prodDescription, category, price, prodQuantity, imgURL
+      FROM products;`;
+      db.query(strQry, (err, results)=> {
+          if(err) throw err;
+          res.status(200).json({results: results})
+      });
+  }
+  fetchProduct(req, res) {
+      const strQry = `SELECT id, prodName, prodDescription, category, price, prodQuantity, imgURL
+      FROM products
+      WHERE id = ?;`;
+      db.query(strQry, [req.params.id], (err, results)=> {
+          if(err) throw err;
+          res.status(200).json({results: results})
+      });
 
+  }
+  addProduct(req, res) {
+      const strQry = 
+      `
+      INSERT INTO Products
+      SET ?;
+      `;
+      db.query(strQry,[req.body],
+          (err)=> {
+              if(err){
+                  res.status(400).json({err: "Unable to insert a new record."});
+              }else {
+                  res.status(200).json({msg: "Product saved"});
+              }
+          }
+      );    
 
+  }
+  updateProduct(req, res) {
+      const strQry = 
+      `
+      UPDATE Products
+      SET ?
+      WHERE id = ?
+      `;
+      db.query(strQry,[req.body, req.params.id],
+          (err)=> {
+              if(err){
+                  res.status(400).json({err: "Unable to update a record."});
+              }else {
+                  res.status(200).json({msg: "Product updated"});
+              }
+          }
+      );    
 
+  }
+  deleteProduct(req, res) {
+      const strQry = 
+      `
+      DELETE FROM Products
+      WHERE id = ?;
+      `;
+      db.query(strQry,[req.params.id], (err)=> {
+          if(err) res.status(400).json({err: "The record was not found."});
+          res.status(200).json({msg: "A product was deleted."});
+      })
+  }
 
-
-
-
-// // import database connection from folder
-// let db = require('./config');
-
-// // import bcrypt
-// let {hash, compare, hashSync} = require('bcrypt');
-
-// // 
-// let {createToken} = require('../middleware/AuthenticatedUser');
-
-// // User class
-// class User {
-//     fetchUsers(req, res){
-//         const qry = 
-//         `
-//         SELECT
-//         userID,
-//         firstName,
-//         lastName,
-//         gender,
-//         emailAdd,
-//         userRole,
-//         userProfile
-//         FROM Users;
-//         `
-//         db.query(strQry, (err, data) => {
-//             if (err) throw err;
-//             else res.status(200).
-//             json({results: data})
-//         })
-//     }
-
-//     fetchUsers(req, res) {
-//         const strQry = 
-//         `
-//         SELECT
-//         userID,
-//         firstName,
-//         lastName,
-//         gender,
-//         emailAdd,
-//         userRole,
-//         userProfile
-//         FROM Users
-//         WHERE userID =?;
-//         `
-//         db.query(strQry, [req.params.id], (err, data) => {
-//             if (err) throw err;
-//             else res.status(200).
-//             json({results: data})
-//         })
-//     }
-
-// }
-
-// // fetchUsers(req, res) {
-// //     const strQry = 
-// //     `
-// //     SELECT
-// //     userID,
-// //     firstName,
-// //     lastName,
-// //     gender,
-// //     emailAdd,
-// //     userRole,
-// //     userProfile
-// //     FROM Users
-// //     WHERE userID =?;
-// //     `
-// //     db.query(strQry, [req.params.id], (err, data) => {
-// //         if (err) throw err;
-// //         else res.status(200).
-// //         json({results: data})
-// //     })
-// // }
-
-
-// class User{
-//     login(req, res) {
-//         const {emailAdd, userPass} = req.body;
-//         const strQry = 
-//         `
-//         SELECT
-//         userID,
-//         firstName,
-//         lastName,
-//         gender,
-//         emailAdd,
-//         userRole,
-//         userPass,
-//         userProfile
-//         FROM Users
-//         WHERE emailAdd = ${emailAdd};
-
-//         `
-//         db.query(strQry, async(err, data) => {
-//         if(err) throw err;
-//             if ((!data) || (data == null)) {
-//                 res.status(401).json({err: 
-//                     "You have entered an invalid email or password"
-//                 });
-//              } else{
-//                 await compare(userPass, data[0].userPass, (cErr, cResult) => {
-//                     if (cErr) throw cErr;
-//                     // Create token
-//                     const jwToken = createToken({emailAdd, userPass});
-//                     // saving the token
-
-//                     res.cookie('Kus Bier', jwToken, {
-//                     maxAge: 3600000,
-//                     httpOnly: true
-//                     })
-//                     if (cResult) {
-//                         res.status(200).
-//                         json({
-//                             msg: 'Login successful',
-//                             jwToken,
-//                             result: data[0]
-//                         })
-//              } else{
-//                 res.status(401).json({
-//                 err: 'you have entered an invalid email or did not register'
-//                 })
-//              }
-
-//                     }
-//                 })
-//              }
-
-//         })
-//     }
-    
-// }
+}
+// Export User class
+module.exports = {
+  User, 
+  Product
+}
